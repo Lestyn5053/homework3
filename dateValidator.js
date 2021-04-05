8d012553256892b1540fc7e389071a9a5349a422
Lestyn Brooks <wiiman98989@gmail.com>
2021-04-05 16:52:55 -0400

    Update dateValidator.js

--- dateValidator.js
+++ dateValidator.js
@@ -14,8 +14,8 @@ module.exports = (req, res, next) => {
       const EpochHeader = parseInt(DateHeader, 10);
       const HeaderUpperLimit = EpochHeader + 300;
       const HeaderLowerLimit = EpochHeader - 300;
-      if (EpochHeader - 300 >= HeaderLowerLimit && EpochHeader + 300 <= HeaderUpperLimit) {
-        req.dateValidation = currentTime;
+      if (HeaderLowerLimit <= currentTime && HeaderUpperLimit >= currentTime) {
+        req.dateValidation = EpochHeader;
         next();
       } else {
         res.sendStatus(StatusCodes.UNAUTHORIZED);
@@ -24,8 +24,8 @@ module.exports = (req, res, next) => {
       const EpochQuery = parseInt(DateQuery, 10);
       const QueryUpperLimit = EpochQuery + 300;
       const QueryLowerLimit = EpochQuery - 300;
-      if (EpochQuery - 300 >= QueryLowerLimit && EpochQuery + 300 <= QueryUpperLimit) {
-        req.dateValidation = currentTime;
+      if (QueryLowerLimit <= currentTime && QueryUpperLimit >= currentTime) {
+        req.dateValidation = EpochQuery;
         next();
       } else {
         res.sendStatus(StatusCodes.UNAUTHORIZED);
@@ -34,8 +34,8 @@ module.exports = (req, res, next) => {
       const Epoch = parseInt(DateHeader, 10);
       const UpperLimit = Epoch + 300;
       const LowerLimit = Epoch - 300;
-      if (Epoch - 300 >= LowerLimit && Epoch + 300 <= UpperLimit) {
-        req.dateValidation = currentTime;
+      if (LowerLimit <= currentTime && UpperLimit >= currentTime) {
+        req.dateValidation = Epoch;
         next();
       } else {
         res.sendStatus(StatusCodes.UNAUTHORIZED);
